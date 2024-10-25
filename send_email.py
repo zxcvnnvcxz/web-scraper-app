863b2cad57f09e2bec0079a3e561d7f5da1a4344
EDWARD <edward.kang0301@gmail.com>
2024-10-26 08:27:16 +1100

    Initial Commit

---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = os.getenv("GMAIL_USER")
+    password = os.getenv("GMAIL_PW")
+
+    receiver = username
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
